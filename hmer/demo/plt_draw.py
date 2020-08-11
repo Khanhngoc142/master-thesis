4f8f93c95d1c3d3893fbf935c9687c2cfd7f7e19
Khanhngoc142 <buikhanhngoc142@gmail.com>
2020-08-11 22:11:12 +0700

    add demo for plt_draw

---
+++ hmer/demo/plt_draw.py
@@ -0,0 +1,16 @@
+import os
+import re
+import matplotlib.pyplot as plt
+
+from extractor.crohme_parser.inkml import Ink
+from utils.plt_draw import plt_draw_traces
+
+if __name__ == "__main__":
+    cur_path = os.path.abspath('.')
+    print(cur_path)
+    root_path = re.findall(r"^(.*hmer).*$", cur_path)[0]
+    ink = Ink(os.path.join(root_path, "data/CROHME_full_v2/CROHME2013_data/TrainINKML/MfrDB/MfrDB0001.inkml"))
+    coords = ink.trace_coords
+    ax = plt_draw_traces(coords)
+    plt.show()
+
