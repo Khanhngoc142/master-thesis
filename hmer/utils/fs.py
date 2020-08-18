3a62a57023959d9618bedcf9c5044f722c3554b8
Khanhngoc142 <buikhanhngoc142@gmail.com>
2020-08-18 18:01:03 +0700

    add save and load object functions

--- hmer/utils/fs.py
+++ hmer/utils/fs.py
@@ -1,8 +1,20 @@
 import os
 import re
+import pickle
 
 
 def get_source_root():
     cur_path = os.path.abspath('.')
     root_path = re.findall(r"^(.*hmer).*$", cur_path)[0]
     return root_path
+
+
+def save_object(obj, file):
+    with open(file, 'wb') as f:
+        pickle.dump(obj, f)
+
+
+def load_object(file):
+    with open(file, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
