2830504dbd8c30b797671504ad59ee68f63b7431
Khanhngoc142 <buikhanhngoc142@gmail.com>
2020-08-20 16:31:08 +0700

    demo

--- hmer/demo/pil_draw.py
+++ hmer/demo/pil_draw.py
@@ -3,17 +3,57 @@ import os
 import matplotlib.pyplot as plt
 
 from extractor.crohme_parser.inkml import Ink
-from utils.image_processing import scale_traces
+from utils.image_processing import *
 from utils.pil_draw import pil_draw_traces
+from utils.plt_draw import *
 from utils.fs import get_source_root
+from extractor.crohme_parser import augmentation
 
 if __name__ == "__main__":
     root_path = get_source_root()
-    ink = Ink(os.path.join(root_path, "data/CROHME_full_v2/CROHME2013_data/TrainINKML/MfrDB/MfrDB0001.inkml"))
-    coords = ink.trace_coords
+    ink = Ink(
+        os.path.join(root_path, "data/CROHME_full_v2/CROHME2013_data/TrainINKML/MfrDB/MfrDB0057.inkml"))  # 57,77,17,27
+    t_coords = ink.trace_coords
     # scale coords
-    _, coords = scale_traces(coords, 300)
-    img = pil_draw_traces(coords)
-    plt.imshow(img, cmap='gray')
-    plt.show()
+    _, t_coords = scale_traces(t_coords, 300)
+    # new_coords = []
+    # for trace_group in ink.trace_groups:
+    # _, scaled_coords = scale_traces(trace_group.trace_coords)
+    # fig = plt.figure()
+    # plt_clear()
+    # fig, ax = plt_setup()
 
+    # fig, axes = plt.subplots(nrows=4)
+    img1 = plt_draw_traces(t_coords)
+    # axes[0].invert_yaxis()
+    # axes[0].set_aspect('equal', adjustable='box')
+    # fig.add_subplot(3, 2, 1)
+    # plt.imshow(img1, cmap='gray')
+    plt.show()
+    for n in range(1, 15):
+        # new_coords = []
+        #
+        # for trace_group in ink.trace_groups:
+        #     i = np.random.uniform(0, 1)
+        #     print(i)
+        #     for trace in trace_group.traces:
+        #         tmp_coords = local_shrink_rotation(1, trace.coords, 3,-10)
+        #         # tmp_coords = local_rotation(tmp_coords, 6)
+        #         # tmp_coords = local_scale(tmp_coords, 1.2)
+        #         new_coords.append(tmp_coords)
+        #
+        # print("\n")
+        # ax = axes[n]
+        # ax.invert_yaxis()
+        # ax.set_aspect('equal', adjustable='box')
+        new_coords = augmentation.InkAugmentor.geometric_transform(ink)
+        print("\n")
+        # plt.show()
+
+        _, coords = scale_traces(new_coords, 300)
+        img = plt_draw_traces(coords)
+        # fig.add_subplot(3,2,n)
+        # plt.imshow(img, cmap='gray')
+        plt.show()
+
+plt.show()
