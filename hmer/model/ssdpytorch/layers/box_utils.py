c7feb68fd8d6631cee8287a8b259aa17c0473d01
Khanhngoc142 <buikhanhngoc142@gmail.com>
2020-09-09 15:10:19 +0700

    add comment

--- hmer/model/ssdpytorch/layers/box_utils.py
+++ hmer/model/ssdpytorch/layers/box_utils.py
@@ -159,11 +159,13 @@ def decode(loc, priors, variances):
 
 
 def log_sum_exp(x):
-    """Utility function for computing log_sum_exp while determining
+    """
+    Utility function for computing log_sum_exp while determining
     This will be used to determine unaveraged confidence loss across
     all examples in a batch.
     Args:
         x (Variable(tensor)): conf_preds from conf layers
+    Alias: cross_entropy_loss
     """
     x_max = x.data.max()
     return torch.log(torch.sum(torch.exp(x-x_max), 1, keepdim=True)) + x_max
