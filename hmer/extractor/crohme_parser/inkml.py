96c5f75e2ae10e398d46bc735b1c513a3be94a62
Khanhngoc142 <buikhanhngoc142@gmail.com>
2020-09-22 16:27:23 +0700

    found a file with inconsistent label - to be = TrainINKML/HAMEX/formulaire034-equation001.inkml. Check back later

--- hmer/extractor/crohme_parser/inkml.py
+++ hmer/extractor/crohme_parser/inkml.py
@@ -93,10 +93,13 @@ class TraceGroup(BaseTrait):
         # if self._label == '+' and not href.startswith('+') and not len(debug_p.findall(href)) > 0:
         #     print("DEBUG +")
 
+        self._discard = False
+
         if self._label == '-' and (href.startswith('\\frac') or href.startswith('_')):
             self._label = '\\frac'
         elif self._label == '-' and href.startswith('='):
-            raise RuntimeError("WHAT A STRANGE CASE.")
+            print("DEBUG: label {}, href {}. DISCARD!".format(self._label, href))
+            self._discard = True
         elif len(debug_p.findall(href)) > 0:
             pass
         elif self._label not in [
@@ -104,8 +107,7 @@ class TraceGroup(BaseTrait):
             '\\rightarrow', '\\neq', '\\exists', '\\sum', '\\int', '+'
         ]:
             if not href.lstrip('\\').startswith(self._label.lstrip('\\')):
-                print(f"WEIRD CASE: `{self._label}` and `{href}`")
-                print("DEBUG")
+                print(f"DEBUG: WEIRD CASE: `{self._label}` and `{href}`")
 
         traces_idx = []
         for trace_view in element.findall(namespace + "traceView"):
@@ -116,6 +118,10 @@ class TraceGroup(BaseTrait):
 
         self._bounding_box = [coord for trace in self._traces for coord in trace.bbox]
         self._bounding_box = [list(np.min(self._bounding_box, axis=0)), list(np.max(self._bounding_box, axis=0))]
+
+    @property
+    def discard(self):
+        return self._discard
 
     @property
     def label(self):
@@ -232,7 +238,7 @@ class Ink(BaseTrait):
 
             trace_groups.sort(key=lambda group: group.bbox[0][0])
 
-            self._trace_groups = trace_groups
+            self._trace_groups = [g for g in trace_groups if not g.discard]
 
             self._simplified_label = simplified_lbl_sep.join([
                 group.label
