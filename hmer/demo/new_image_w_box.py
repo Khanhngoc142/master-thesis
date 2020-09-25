a58025769a87fe25dbe2387282ba5c976894daea
Khanhngoc142 <buikhanhngoc142@gmail.com>
2020-09-25 15:00:06 +0700

    update to run for predict results

--- hmer/demo/new_image_w_box.py
+++ hmer/demo/new_image_w_box.py
@@ -3,13 +3,19 @@ from utilities.fs import get_path
 from utilities import plt_draw
 
 
-def process_line(l):
-    l = l.strip().split()
-    img_path = l[0]
-    l = np.array(l[1:]).reshape((-1, 5))
-    cls_ = l[:, 0]
-    coords_ = l[:, 1:]
-    conf_ = np.ones(cls_.shape, dtype=np.float)
+def process_line(line, ground_truth=False):
+    line = line.strip().split()
+    img_path = line[0]
+    if ground_truth:
+        line = np.array(line[1:]).reshape((-1, 5))
+        cls_ = line[:, 0]
+        coords_ = line[:, 1:]
+        conf_ = np.ones(cls_.shape, dtype=np.float)
+    else:
+        line = np.array(line[1:]).reshape((-1, 6))
+        cls_ = line[:, 0]
+        coords_ = line[:, 2:]
+        conf_ = line[:, 1]
     return img_path, (cls_.astype('int'), conf_.astype('float'), coords_.astype('float'))
 
 
@@ -18,7 +24,7 @@ if __name__ == "__main__":
     with open(label_file, 'r') as f:
         data = f.readlines()
 
-    data = dict([process_line(line) for line in data])
+    data = dict([process_line(line, ground_truth=True) for line in data])
     chosen_img = "training/data/CROHME_2013_valid/rit_42160_4.png"
 
     classes, conf, boxes = data[chosen_img]
