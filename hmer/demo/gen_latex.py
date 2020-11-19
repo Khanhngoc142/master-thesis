753df95445c39977dc28f4931e6136f3d72839c6
ngocbk <ngocbk@vng.com.vn>
2020-11-19 16:12:47 +0700

    add code to gen latex from ssd results

---
+++ hmer/demo/gen_latex.py
@@ -0,0 +1,33 @@
+from utilities import BB_To_Tree
+import timeout_decorator
+
+
+@timeout_decorator.timeout(180, exception_message='Time out', timeout_exception=TimeoutError)
+def latex_generator(line, is_pred=True):
+    generator = BB_To_Tree.BBParser()
+    return generator.process(line, True)
+
+
+if __name__ == "__main__":
+    # generator = BB_To_Tree.BBParser()
+    predicted_file = '/home/ubuntu/data/ssd_final_result/aug_geo_extra_data_test_model24_multithreshnmstest.txt'
+
+    with open(predicted_file) as f:
+        lines = f.readlines()
+        # latex_result = [line.split(' ')[0].replace('.png', '') + '\t' + latex_generator(line, True) for line in lines]
+        latex_result = []
+        err_result = []
+        for line in lines:
+            file_name = line.split(' ')[0].replace('.png', '')
+            try:
+                latex_result.append(file_name + '\t' + latex_generator(line, True))
+            except TimeoutError:
+                err_result.append(file_name)
+
+    with open('/home/ubuntu/workspace/mine/master-thesis.git/hmer/metadata/latex_files/predicted_latex_aug_geo_sinh_tm180s.txt',
+              'w') as wf:
+        wf.write('\n'.join(latex_result))
+
+    with open('/home/ubuntu/workspace/mine/master-thesis.git/hmer/metadata/latex_files/timeout_files_aug_geo_sinh_tm180s.txt',
+              'w') as wf:
+        wf.write('\n'.join(err_result))
