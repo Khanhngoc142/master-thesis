c199c7a4d5f27b874bcbd6358bc4492e7f9f5984
ngocbk <ngocbk@vng.com.vn>
2021-02-25 15:59:22 +0700

    evaluate 3 models

--- hmer/demo/evaluate_model.py
+++ hmer/demo/evaluate_model.py
@@ -3,16 +3,16 @@ from utilities.metrics.simple_mAP import evaluate_AP
 
 
 if __name__ == '__main__':
-    # pred_path_aug = "/home/ubuntu/data/v_filter0.1_globalnms0.650.15_testssd0.10.1_modelvisdombest/aug_geotest.txt"
-    # pred_path_baseline = "/home/ubuntu/data/v_filter0.1_globalnms0.650.15_testssd0.10.1_modelvisdombest/baselinetest.txt"
-    pred_path_extra = '/home/ubuntu/data/aug_extra_data_filter02test.txt'
+    pred_path_aug = "/home/ubuntu/data/ssd_final_result/aug_geo_multithreshnmstest.txt"
+    pred_path_baseline = "/home/ubuntu/data/ssd_final_result/baseline_multithreshnmstest.txt"
+    pred_path_extra = '/home/ubuntu/data/ssd_final_result/aug_geo_extra_data_test_model24_multithreshnmstest.txt'
     gt_path = "/home/ubuntu/workspace/mine/master-thesis.git/hmer/training/data/CROHME_2013_test/labels_old.txt"
 
-    # eval_dict_aug, pd_aug_info = evaluate_AP(gt_path, pred_path_aug)
-    # eval_dict_baseline, pd_base_info = evaluate_AP(gt_path, pred_path_baseline)
+    eval_dict_aug, pd_aug_info = evaluate_AP(gt_path, pred_path_aug)
+    eval_dict_baseline, pd_base_info = evaluate_AP(gt_path, pred_path_baseline)
     eval_dict_extra, pd_extra_info = evaluate_AP(gt_path, pred_path_extra)
 
-    # mAP_aug = np.array(list(eval_dict_aug.values())).mean()
-    # mAP_baseline = np.array(list(eval_dict_baseline.values())).mean()
+    mAP_aug = np.array(list(eval_dict_aug.values())).mean()
+    mAP_baseline = np.array(list(eval_dict_baseline.values())).mean()
     mAP_extra = np.array(list(eval_dict_extra.values())).mean()
-    print(mAP_extra)
+    print(mAP_baseline, mAP_aug, mAP_extra)
